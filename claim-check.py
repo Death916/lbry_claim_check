22ac8755805682ed6e0d52d3210dedec8bcb6e64
Trent Nelson <tavn1992@gmail.com>
2020-06-08 14:53:10 -0700

    check for proper name

--- claim-check.py
+++ claim-check.py
@@ -6,18 +6,24 @@ not_ctrl = {}
 
 
 # gets list of claims for specific channel
-# only works for own channel currently
+# only correct for for own channel currently
 
 try:
-    
-    channel = input('please enter a channel name starting with @: ')
+    while True:
+        channel = input('please enter a channel name starting with @: ')
+        if channel.startswith('@') == False:
+            print('enter a valid channel name ')
+            channel = input('please enter a channel name starting with @ ')
+        if channel.startswith('@') == True:
+            break
 except:
     print('Enter a valid channel name starting with @')
-    
+
+
 def claim_search(channel):
     try:
-        search = requests.post("http://localhost:5279", json={"method": "claim_search", "params": 
-        {"channel": channel}}).json()
+        search = requests.post("http://localhost:5279", json={"method": "claim_search", "params":
+                                                              {"channel": channel}}).json()
     except:
         print('search failed')
         pass
@@ -38,11 +44,12 @@ def get_data(search):
         title = not_ctrl['value']['title']
 
         if claim["meta"]['is_controlling'] == False:
-            
+
             print(amount)
             print('you are not controlling ' + title)
         else:
             print('You are controlling ' + title)
+
 
 search_result = claim_search(channel)
 
