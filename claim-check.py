757cdae3e415a4a49a9971e1d42bf9468ebf29ff
Trent Nelson <tavn1992@gmail.com>
2020-06-08 04:50:32 -0700

    functions and input channel prompt

--- claim-check.py
+++ claim-check.py
@@ -3,33 +3,46 @@ import json
 
 channel = ""
 not_ctrl = {}
-cntrl = []
 
+
+# gets list of claims for specific channel
+# only works for own channel currently
+
+try:
+    
+    channel = input('please enter a channel name starting with @: ')
+except:
+    print('Enter a valid channel name starting with @')
+    
 def claim_search(channel):
     try:
         search = requests.post("http://localhost:5279", json={"method": "claim_search", "params": 
-        {"channel": "@freestyle"}}).json()
+        {"channel": channel}}).json()
     except:
         print('search failed')
         pass
     return search
 
 
-version = requests.post("http://localhost:5279", json={"method": "version", "params": {}}).json()
+#version = requests.post("http://localhost:5279", json={"method": "version", "params": {}}).json()
 
-#print(version)
+# print(version)
 
+# parses the data for relevant info
 def get_data(search):
-    
+
     result = search["result"]["items"]
     for claim in result:
-        
+        not_ctrl = dict(claim)
+        amount = not_ctrl['meta']['effective_amount']
+        title = not_ctrl['value']['title']
+
         if claim["meta"]['is_controlling'] == False:
-            not_ctrl = dict(claim)
-            amount = not_ctrl['meta']['effective_amount']
-            print(amount)  
-            title = not_ctrl['value']['title']
-            print(title)
+            
+            print(amount)
+            print('you are not controlling ' + title)
+        else:
+            print('You are controlling ' + title)
 
 search_result = claim_search(channel)
 
