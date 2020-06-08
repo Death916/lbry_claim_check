c6581f1c9f4aee67b81e4353bc84bf0de66de3cd
Trent Nelson <tavn1992@gmail.com>
2020-06-08 04:05:53 -0700

    add functions

--- claim-check.py
+++ claim-check.py
@@ -5,25 +5,32 @@ channel = ""
 not_ctrl = {}
 cntrl = []
 
-
-try:
-    search = requests.post("http://localhost:5279", json={"method": "claim_search", "params": 
-    {"channel": "@freestyle"}}).json()
-except:
-    print('search failed')
-    pass
+def claim_search(channel):
+    try:
+        search = requests.post("http://localhost:5279", json={"method": "claim_search", "params": 
+        {"channel": "@freestyle"}}).json()
+    except:
+        print('search failed')
+        pass
+    return search
 
 
 version = requests.post("http://localhost:5279", json={"method": "version", "params": {}}).json()
 
 #print(version)
 
-
-result = search["result"]["items"]
-for claim in result:
+def get_data(search):
     
-    if claim["meta"]['is_controlling'] == False:
-        not_ctrl = dict(claim)
-        amount = not_ctrl['meta']['effective_amount']
-        print(amount)  
-        title = not_ctrl['value']['title']
+    result = search["result"]["items"]
+    for claim in result:
+        
+        if claim["meta"]['is_controlling'] == False:
+            not_ctrl = dict(claim)
+            amount = not_ctrl['meta']['effective_amount']
+            print(amount)  
+            title = not_ctrl['value']['title']
+            print(title)
+
+search_result = claim_search(channel)
+
+get_data(search_result)
