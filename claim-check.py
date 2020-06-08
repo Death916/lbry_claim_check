edf571c8ec905d197b2b1a6ecdbfc2cb97e709d9
Trent Nelson <tavn1992@gmail.com>
2020-06-08 04:02:53 -0700

    found proper keys

--- claim-check.py
+++ claim-check.py
@@ -2,21 +2,28 @@ import requests
 import json
 
 channel = ""
-not_ctrl = []
+not_ctrl = {}
 cntrl = []
 
 
 try:
     search = requests.post("http://localhost:5279", json={"method": "claim_search", "params": 
-    {"channel": "@freestyle", "is_controlling":"false"}}).json()
-except:
-    pass
-try:
-    print(search)
+    {"channel": "@freestyle"}}).json()
 except:
     print('search failed')
     pass
 
+
 version = requests.post("http://localhost:5279", json={"method": "version", "params": {}}).json()
-print('im here')
-print(version)
+
+#print(version)
+
+
+result = search["result"]["items"]
+for claim in result:
+    
+    if claim["meta"]['is_controlling'] == False:
+        not_ctrl = dict(claim)
+        amount = not_ctrl['meta']['effective_amount']
+        print(amount)  
+        title = not_ctrl['value']['title']
